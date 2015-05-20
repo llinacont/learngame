ca0675109d317998c2d886819c3948dfd8f624f5
llinacont <llinacont@gmail.com>
2015-05-20 17:02:48 +0200

    First commit

---
+++ android/src/com/learn/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.learn.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.learn.game.LearnGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LearnGame(), config);
+	}
+}
